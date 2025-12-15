2b35676b1fd217b7b83081501de7e0d6ae34fe59
rohitadhikarii28 <rohitadhikarii28@gmail.com>
2025-12-14 19:39:46 -0600

    runtime issues- removed "reusePort: true"

--- server/index.ts
+++ server/index.ts
@@ -137,15 +137,15 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const PORT = 5000;
+  const HOST = "127.0.0.1";
   httpServer.listen(
     {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
+      port: PORT,
+      host:HOST,
     },
     () => {
-      log(`serving on port ${port}`);
+      log(`serving on port ${PORT}`);
     },
   );
 })();
